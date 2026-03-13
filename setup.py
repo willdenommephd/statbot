08bb650f691f7df2cdca887f7b5c90b3c331383c
William Denomme <willdenomme@outlook.com>
2026-03-13 15:22:22 -0400

    Finalize package metadata and add license

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="statbot",
     version="0.1.0",
     description="StatBot: Automated statistical data extraction and Excel reporting tool",
-    author="willdenommephd",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="William James Denomme",
+    author_email="willdenomme@outlook.com",
+    url="https://github.com/willdenommephd/statbot",
+    license="MIT",
     packages=find_packages(),
     install_requires=[
         "selenium",
@@ -18,4 +26,11 @@ setup(
             'statbot=main:main',
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
